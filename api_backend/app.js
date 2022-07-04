b81204c2e6e143d3a9571a3e387eb3adab53d419
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-07-04 16:27:01 -0300

    Update ONE

--- api_backend/app.js
+++ api_backend/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-app.use(express.json());
 const Anuncio = require('./models/Anuncio');
 //const db = require("./models/db");
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+  app.use(cors());
+  next();
+});
+
+
 
 app.get('/', async (req, res) => {
   await Anuncio.findAll({order: [['id', 'DESC']]}).then(function(anuncios){
