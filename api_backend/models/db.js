51a953ac79fc001559f0c2283d3d60f249d417b2
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-07-04 11:30:04 -0300

    Conexão com o banco de dados

--- api_backend/models/db.js
+++ api_backend/models/db.js
@@ -2,7 +2,12 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('imersao_sete', 'root', '', {
     host: 'localhost',
-    dialect: 'mysql2'
+    dialect: 'mysql'
 });
+
+sequelize.authenticate()
+.then(function(){
+    console.log("Conexão com o banco de dados realizada com sucesso!");
+})
 
 module.exports = sequelize;
