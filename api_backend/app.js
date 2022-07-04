83ed47ad3ee217f9bb239432924f34510070676e
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-07-04 15:45:52 -0300

    Update

--- api_backend/app.js
+++ api_backend/app.js
@@ -1,11 +1,7 @@
 const express = require('express');
-//const { json } = require('sequelize');
 const app = express();
-
 app.use(express.json());
-
 const Anuncio = require('./models/Anuncio');
-
 //const db = require("./models/db");
 
 app.get('/', function (req, res) {
@@ -15,8 +11,19 @@ app.get('/', function (req, res) {
 });
 
 /* Visualizar */
-app.get('/visualizar', async (req, res) => {
-  res.send("Visualizar");
+app.get('/visualizar/:id', async (req, res) => {
+  Anuncio.findByPk(req.params.id)
+  .then(anuncio => {
+    return res.json({
+      error: false,
+      anuncio
+    })
+  }).catch(function(erro){
+    return res.status(400).json({
+      error: true,
+      message: "Erro: Anúncio não encontrado" 
+    });
+  });
 });
 
 
@@ -37,6 +44,25 @@ app.post('/cadastrar', async (req, res) => {
     });
   });
 });
+
+
+/* Editar */
+app.put('/editar', (req, res) => {
+  Anuncio.update(req.body, {
+    where: {id: req.body.id}
+  }).then(function() {
+    return res.json({
+      error: false,
+      message: "Anúncio editado com sucesso!"
+    });
+  }).catch(function(erro){
+    return res.status(400).json({
+      error: false,
+      message: "Erro> Anúncio não foi editado!"
+    });
+  });
+});
+
 
 app.listen(8080, function(){
     console.log("Servidor iniciado na porta 8080: http://localhost:8080/");
