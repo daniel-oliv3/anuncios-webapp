8ed87bdc354225ec7a962ade368772c8f85fbdf6
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-07-05 08:55:17 -0300

    Up

--- crud_frontend/src/App.js
+++ crud_frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Visualizar } from './pages/Visualizar';
+import { Cadastrar } from "./pages/Cadastrar";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/visualizar" component={Visualizar} />
+          <Route path="/cadastrar" component={Cadastrar} />
         </Switch>
       </Router>
     </div>
