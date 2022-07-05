a6d3c7f0825959f987619e836678c2366a625c8a
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-07-05 08:58:37 -0300

    Create component editar and index.js

--- crud_frontend/src/App.js
+++ crud_frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Visualizar } from './pages/Visualizar';
 import { Cadastrar } from "./pages/Cadastrar";
+import { Editar } from "./pages/Editar";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/visualizar" component={Visualizar} />
           <Route path="/cadastrar" component={Cadastrar} />
+          <Route path="/editar" component={Editar} />
         </Switch>
       </Router>
     </div>
