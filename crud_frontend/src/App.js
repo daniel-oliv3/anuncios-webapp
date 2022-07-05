209100d5e72f17d49887d539a3a155e5750bc65d
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-07-05 08:49:57 -0300

    Create component cadastrar and index.js

--- crud_frontend/src/App.js
+++ crud_frontend/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Home } from './pages/Home/index';
+import { Home } from './pages/Home';
+import { Visualizar } from './pages/Visualizar';
 
 function App() {
   return (
     <div>
       <Router>
         <Switch>
-          <Route path="/" component={Home} />
+          <Route exact path="/" component={Home} />
+          <Route path="/visualizar" component={Visualizar} />
         </Switch>
       </Router>
     </div>
---
+++ crud_frontend/src/pages/Cadastrar/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Cadastrar = () => {
+  return (
+    <div>
+      <h1>Cadastrar Anúncio</h1>
+    </div>
+  );
+}
