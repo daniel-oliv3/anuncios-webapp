6ea1904a3b2e1df98cb75330ae49832955848a62
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-07-06 17:38:34 -0300

    Table

--- crud_frontend/src/pages/Home/index.js
+++ crud_frontend/src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from 'reactstrap';
+import { Container, Table } from 'reactstrap';
 
 export const Home = () => {
   return (
@@ -10,11 +10,40 @@ export const Home = () => {
             <h1>Anúncios</h1>
           </div>
           <div className="p-2">
-            <a href="/cadastrar" className="btn btn-outline-success btn-sm">Enviar</a>
+            <a href="/cadastrar" className="btn btn-outline-success btn-sm">Cadastrar</a>
           </div>
 
         </div>
-        
+        <Table striped>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+          </tbody>
+        </Table>
       </Container>
     </div>
   );
